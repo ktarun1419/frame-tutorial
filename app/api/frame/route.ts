b6af9ab284052813d141b2a72e5c789ff2500dde
ktarun1419 <tarunkaushik355@gmail.com>
2024-02-04 02:33:23 +0530

    chnages

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -11,15 +11,12 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
       return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame 7</title>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qme4FXhoxHHfyzTfRxSpASbMF8kajLEPkRQWhwWu9pkUjm/7.png" />
-    <meta property="fc:frame:button:1" content="Cosmic Cowboys" />
-    <meta property="fc:frame:button:1:action" content="post_redirect" />
-    <meta property="fc:frame:button:2" content="Blog post Tutorial" />
-    <meta property="fc:frame:button:2:action" content="post_redirect" />
-    <meta property="fc:frame:button:3" content="Video Tutorial" />
-    <meta property="fc:frame:button:3:action" content="post_redirect" />
-    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
-  </head></html>`);
+    <meta property="fc:video" content="https://sample-videos.com/video321/mp4/720/big_buck_bunny_720p_1mb.mp4">
+    <meta property="fc:video:width" content="640">
+    <meta property="fc:video:height" content="360">
+    <meta property="fc:video:type" content="video/mp4">
+  </head>
+  </html>`);
   } else {
   return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame ${id}</title>
@@ -36,3 +33,11 @@ export async function POST(req: NextRequest): Promise<Response> {
 }
 
 export const dynamic = 'force-dynamic';
+// <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qme4FXhoxHHfyzTfRxSpASbMF8kajLEPkRQWhwWu9pkUjm/7.png" />
+// <meta property="fc:frame:button:1" content="Cosmic Cowboys" />
+// <meta property="fc:frame:button:1:action" content="post_redirect" />
+// <meta property="fc:frame:button:2" content="Blog post Tutorial" />
+// <meta property="fc:frame:button:2:action" content="post_redirect" />
+// <meta property="fc:frame:button:3" content="Video Tutorial" />
+// <meta property="fc:frame:button:3:action" content="post_redirect" />
+// <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
